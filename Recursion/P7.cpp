f59767a93aea82d589b973e82afffcc37e71bbde
Parth Garg <gargp6995@gmail.com>
2026-02-19 23:29:07 +0530

    Add files via upload

---
+++ Recursion/P7.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int n)
+{
+    if (n==0)      
+        return;
+    return n+f(n-1);
+    
+}
+int main() {
+    int n;
+    cin>>n;
+    cout<<f(n);
+    return 0;
+}
